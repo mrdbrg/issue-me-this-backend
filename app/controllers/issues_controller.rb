8f297688bd4eb2fe2f9b82e12facc9765fe12508
github username <github email address>
2020-07-28 01:04:09 -0400

    IFinis show action - Issue controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,18 +6,21 @@ class IssuesController < ApplicationController
   end
 
   def create
-    # issue = Issue.create(
-        # title: params[:title], 
-        # issue_body: params[:issue_body], 
-        # issue: params[:id], 
-        # user: params[:user_id]
-        # )
-    # byebug
+    newIssue = Issue.create(
+        title: params[:title], 
+        issue_body: params[:issue_body], 
+        user: User.all.sample # selects a random user
+      )
+
+    if newIssue.valid?
+      render json: newIssue
+    else
+      render json: { message: "Something went wrong while creating this issue."}, status: 400
+    end
   end
 
   def show
     myIssue = Issue.find_by(id: params[:id])
-    # byebug
     render json: myIssue
   end
 end
