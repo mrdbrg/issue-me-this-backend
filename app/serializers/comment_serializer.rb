47cf87b467de7779d01df4984be1ab756bc4f9fb
github username <rdg.brg@gmail.com>
2020-11-28 15:00:36 -0500

    Add like_comments column to  the comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment_body, :syntax, :issue_id
+  attributes :id, :comment_body, :syntax, :issue_id, :like_comments
   belongs_to :user
 
   # # Source Article for the code below: https://medium.com/@stellamarisnjage/serializing-deeply-nested-associations-in-rails-2173d588c528
   # def user
-  #   ActiveModel::SerializableResource.new(object.user,  each_serializer: UserSerializer)
+  #   ActiveModel::SerializableResource.new(object.user, each_serializer: UserSerializer)
   # end
 end
