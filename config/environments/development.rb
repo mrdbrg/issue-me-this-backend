de2119d1a467304b377d748d34bd8f3bfb825ed2
github username <rdg.brg@gmail.com>
2020-12-02 15:10:22 -0500

    No changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,14 +25,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # This like tells ActiveRecord to use cloudinary to store your files.
-  config.active_storage.service = :cloudinary
-
-  # Set up Rails to the port you will be using.
-  Rails.application.routes.default_url_options = { host: "http://localhost:3001" }
-
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+
+  Rails.application.routes.default_url_options = { host: "http://localhost:3001" }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
