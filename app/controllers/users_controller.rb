f911566a050916c4deb2535fd4ceb78dd7c6c0b6
github username <rdg.brg@gmail.com>
2020-12-05 15:57:47 -0500

    Fix update password bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,11 +26,7 @@ class UsersController < ApplicationController
       
       path = Rails.root + "public/images/default-profile.jpg"
       @user.profile_picture.attach(io: File.open(path), filename: "#{@user.last_name}-#{@user.email}.jpg")      
-
-
       @user.save
-
-
        # if user is valid collect skills from params
       top_skills = Skill.all.find_all { |skill| params[:top_skills].include?(skill[:text]) }
       # if user is valid create association between new user and skills
@@ -40,18 +36,15 @@ class UsersController < ApplicationController
           skill: sk
         )
       end
-
-
       # encrypt the user id ====> token = JWT.encode payload, password parameter, 'algorithm'
       token = JWT.encode({ user_id: @user.id }, "not_too_safe", "HS256")
 
-
       # if it validates to true renders json: user & token ====> run user explicitly through serializer
-      render json: { user: UserSerializer.new(@user), token: token, errorStatus: false }
+      render json: { user: UserSerializer.new(@user), token: token, errorStatus: false }, status: :accepted
     else
       # if user is not valid - render error messages (rails validation messages) and status code
 
-      render json: { header: "You need to fulfill these #{@user.errors.full_messages.count} requirements", error: @user.errors.full_messages, errorStatus: true }, status: :bad_request 
+      render json: { header: "You need to fulfill these #{@user.errors.full_messages.count} requirements", error: @user.errors.full_messages, errorStatus: true }, status: :unauthorized 
     end
   end
 
@@ -60,23 +53,21 @@ class UsersController < ApplicationController
     new_skills = []
     remove_skills = []
 
-    if params[:password] == nil
-      password = user[:password_digest]
-    else
-      password = params[:password]
-    end
-
     user.update(
       email: params[:email], 
       first_name: params[:first_name], 
       last_name: params[:last_name], 
       job_title: params[:job_title], 
-      birthday: params[:birthday], 
-      password: password 
+      birthday: params[:birthday]
     )
 
-    if user.valid?
+    if params[:password] != nil
+      user.update(
+        password: params[:password]
+      )
+    end
 
+    if user.valid?
     
       if params[:remove_skills].count != 0
         remove_skills = params[:remove_skills]
